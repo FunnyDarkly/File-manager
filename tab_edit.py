307029cb3190b2818e3bd4acba151ead8f2a66e7
FunnyDarkly <35658589+FunnyDarkly@users.noreply.github.com>
2024-12-11 08:46:20 +0600

    Add files via upload

--- tab_edit.py
+++ tab_edit.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_TabEdit(object):
+class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(230, 200)
